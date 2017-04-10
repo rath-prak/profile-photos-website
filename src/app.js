bda8f5f3d80adbd8fcff56f64a8c04f488ef67f8
rath-prak <rathanap@gmail.com>
2017-04-10 21:25:13 +1200

    add masonry and new images

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 import css from './app.scss';
-// require('./js/vendor/masonry');
+
+var jQueryBridget = require('jquery-bridget');
+var Masonry = require('masonry-layout');
+// make Masonry a jQuery plugin
+jQueryBridget( 'masonry', Masonry, $ );
+// now you can use $().masonry()
+$('.grid').masonry({
+    itemSelector: '.grid-item',
+});
