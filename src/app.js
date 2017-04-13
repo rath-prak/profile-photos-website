002341562d8030805613da212f39d760ddaed5cf
rath-prak <rathanap@gmail.com>
2017-04-13 15:10:22 +1200

    fix navbar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import css from './app.scss';
 
-<<<<<<< HEAD
-=======
 
 // var $ = require('jquery');
 
->>>>>>> ee021bda68194cda1765ad11e179b430c76fdd02
 var jQueryBridget = require('jquery-bridget');
 var Masonry = require('masonry-layout');
 // make Masonry a jQuery plugin
